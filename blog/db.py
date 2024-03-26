60cfbafeb5dce452019f9f9546bdf4eaa1ca4f34
ada-fruit <emjorg96@gmail.com>
2024-03-26 11:43:59 -0500

    Replace deprecated sqlite3 datetime converters

--- blog/db.py
+++ blog/db.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlite3
 
 import click
@@ -49,3 +50,45 @@ def init_app(app):
 def init_db_command():
     init_db()
     click.echo('Initialized the database.')
+
+
+# Add some recipes for adapters and converters.
+# The default sqlite3 timestamp converter is deprecated.
+def adapt_date_iso(val):
+    """Adapt datetime.date to ISO 8601 date."""
+    return val.isoformat()
+
+
+def adapt_datetime_iso(val):
+    """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
+    return val.isoformat()
+
+
+def adapt_datetime_epoch(val):
+    """Adapt datetime.datetime to Unix timestamp."""
+    return int(val.timestamp())
+
+
+sqlite3.register_adapter(datetime.date, adapt_date_iso)
+sqlite3.register_adapter(datetime.datetime, adapt_datetime_iso)
+sqlite3.register_adapter(datetime.datetime, adapt_datetime_epoch)
+
+
+def convert_date(val):
+    """Convert ISO 8601 date to datetime.date object."""
+    return datetime.date.fromisoformat(val.decode())
+
+
+def convert_datetime(val):
+    """Convert ISO 8601 datetime to datetime.datetime object."""
+    return datetime.datetime.fromisoformat(val.decode())
+
+
+def convert_timestamp(val):
+    """Convert Unix epoch timestamp to datetime.datetime object."""
+    return datetime.datetime.fromtimestamp(int(val))
+
+
+sqlite3.register_converter("date", convert_date)
+sqlite3.register_converter("datetime", convert_datetime)
+sqlite3.register_converter("timestamp", convert_timestamp)
