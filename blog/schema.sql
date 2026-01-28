fe640320df9e80d00f1847e8406c6aeafbad8543
Elizabeth Jorgensen <161362020+ada-fruit@users.noreply.github.com>
2026-01-28 15:01:41 -0600

    Make schema nicer for Postgres install
    Adds some "if not exists" clauses so that you don't have to run the
    schema file twice when installing Postgres for local development.

--- blog/schema.sql
+++ blog/schema.sql
@@ -1,18 +1,18 @@
-drop table blog_user;
-drop table newsletter_subscriber;
-drop table post;
+drop table if exists blog_user;
+drop table if exists newsletter_subscriber;
+drop table if exists post;
 
-create table blog_user (
+create table if not exists blog_user (
     id serial primary key,
     username text unique not null,
     password text not null
 );
 
-create table newsletter_subscriber (
+create table if not exists newsletter_subscriber (
     email text unique not null
 );
 
-create table post (
+create table if not exists post (
     id serial primary key,
     author integer references blog_user,
     created timestamp not null default current_timestamp,
